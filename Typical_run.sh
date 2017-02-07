889356bd907ee0a7e9a91f1be7eae34367673eb3
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-02-07 09:54:11 +0000

    Update Typical_run.sh

--- Typical_run.sh
+++ Typical_run.sh
@@ -1,4 +1,13 @@
 #split
+$STRAW_DN/Denovo-assembly_pipeline/scripts/PIPELINE.sh -c splitfq \
+  $STRAW_DN/../raw/D2_1.fq.gz \
+  5000000 \
+  $STRAW_DN/split
+
+$STRAW_DN/Denovo-assembly_pipeline/scripts/PIPELINE.sh -c splitfq \
+  $STRAW_DN/../raw/D2_2.fq.gz \
+  5000000 \
+  $STRAW_DN/split
 
 #trim
 for R1 in $STRAW_DN/split/D2_1.fq.gz.aaaa*; do
@@ -12,6 +21,16 @@ for R1 in $STRAW_DN/split/D2_1.fq.gz.aaaa*; do
 done
 
 #clean
+for R1 in $STRAW_DN/trimmed/D2_1.fq.gz.aaaa*; do
+  R2=$(echo $R1|sed 's/_1/_2/');
+  $STRAW_DN/Denovo-assembly_pipeline/scripts/PIPELINE.sh -c clean \
+  $R1 \
+  $R2 \
+  $STRAW_DN/trimmed \
+  $STRAW_DN/Denovo-assembly_pipeline/scripts/truseq.fa \
+  4
+done
+
 
 #filter
 for R1 in $STRAW_DN/cleaned/D2_1.fq.gz.aaaa*.f.*; do 
