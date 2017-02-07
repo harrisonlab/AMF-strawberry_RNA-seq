c2f401598947ec7628bf99706f4d2ce70a5c28c0
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-02-07 09:39:46 +0000

    Update Typical_run.sh

--- Typical_run.sh
+++ Typical_run.sh
@@ -1,14 +1,25 @@
-for R1 in /home/deakig/projects/strawberry/data/De_novo_assembly/split/D2_1.fq.gz.aaaa*; do
+#split
+
+#trim
+for R1 in $STRAW_DN/split/D2_1.fq.gz.aaaa*; do
   R2=$(echo $R1|sed 's/_1/_2/');
-  /home/deakig/pipelines/Denovo-assembly/scripts/PIPELINE.sh -c trim \
-  
+  $STRAW_DN/Denovo-assembly_pipeline/scripts/PIPELINE.sh -c trim \
+  $R1 \
+  $R2 \
+  $STRAW_DN/trimmed \
+  $STRAW_DN/Denovo-assembly_pipeline/scripts/truseq.fa \
+  4
+done
 
+#clean
 
-for R1 in /home/deakig/projects/strawberry/data/De_novo_assembly/cleaned/D2_1.fq.gz.aaaa*.f.*; do 
+#filter
+for R1 in $STRAW_DN/cleaned/D2_1.fq.gz.aaaa*.f.*; do 
   R2=$(echo $R1|sed 's/\.f\./\.r\./'); 
-  /home/deakig/pipelines/Denovo-assembly/scripts/PIPELINE.sh -c filter \
+  $STRAW_DN/Denovo-assembly_pipeline/scripts/PIPELINE.sh -c filter \
   /home/deakig/projects/strawberry/data/contaminants/contaminants \
   /home/deakig/projects/strawberry/data/De_novo_assembly/filtered \
   $R1 $R2
 done
 
+#normalise
