bca1f0d950ef3dc2b8494624d097d0248669b3ab
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-03-30 16:09:33 +0100

    Update Typical_denovo_run.sh

--- Typical_denovo_run.sh
+++ Typical_denovo_run.sh
@@ -220,4 +220,13 @@ done
 # usearch9 -cluster_fast in.fa -id 1 -strand plus -sort length -centroids out.fa
 # $STRAW_DN/Denovo-assembly/scripts/dereplicate.pl| \
 
- 
+# Combine filtered transcripts and filter round 2
+cat soap/ trinity/ oases/ trans/ combined.fa > combined.fa
+$STRAW_DN/Denovo-assembly/scripts/sort_fasta > combined_sorted.fa #filters out less than 75 length
+cd-hit-est -c 1.0 -i combined_sorted.fa -M 0 -T 8 -o combined.defrag.fa 
+
+usearch9 -makeudb_ublast combined.defrag.fa  -output db.udb # likely to very close to 32 bit usearch mem limit
+usearch9 -ublast combined.defrag.fa -db db.udb -id 0.97 -evalue 1e-2 -accel 0.06 -strand plus -userout res2.uo  -userfields query+target+clusternr+id+ql+tl
+awk -F"\t" '{a=substr($1,length($1),1);b=substr($1,length($1)-4,1);if(a=="C"&&b!="-"){x="C"}else{x="I"};$(NF+1)=x;a=substr($2,length($2),1);b=substr($2,length($2)-4,1);if(a=="C"&&b!="-"){x="C"}else{x="I"};$(NF+1)=x;print}' OFS="\t" res2.uo >res.f.uo
+
+# then process in R 
