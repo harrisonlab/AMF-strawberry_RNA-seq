1c805d211b55d5f6604e9ee4196d43a0ca32b92c
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-04-07 17:01:32 +0100

    Update Typical_denovo_run.sh

--- Typical_denovo_run.sh
+++ Typical_denovo_run.sh
@@ -213,24 +213,23 @@ $STRAW_DN/Denovo-assembly_pipeline/scripts/PIPELINE.sh -c post \
 done
 
 
- $STRAW_DN/Denovo-assembly/scripts/dereplicate.pl trinity_D1.Trinity.fasta > out1.fa
- $STRAW_DN/Denovo-assembly/scripts/get_longest_cds.pl out1.fa >cds.fa
- cd-hit-est -c 1.0 -i cds.fa -M 0 -T 8 -o cds.defrag.fa
+# $STRAW_DN/Denovo-assembly/scripts/dereplicate.pl trinity_D1.Trinity.fasta > out1.fa
+# $STRAW_DN/Denovo-assembly/scripts/get_longest_cds.pl out1.fa >cds.fa
+# cd-hit-est -c 1.0 -i cds.fa -M 0 -T 8 -o cds.defrag.fa
  #$STRAW_DN/Denovo-assembly/scripts/sort_fasta.pl cds.defrag.fa >cds.defrag.sorted.fa
 # $STRAW_DN/Denovo-assembly/scripts/dereplicate.pl| \
 
 # Combine filtered transcripts and filter round 2 
 # First step is to check for complete cds at least 75 
-cat*C35*.fa | grep -P -A1 --no-group-separator "_\d*_3C$" | \
-$STRAW_DN/Denovo-assembly_pipeline/scripts/sort_fasta | \
+cat *C35*.fa | grep -P -A1 --no-group-separator "_\d*_3C$" | \
 awk -F"_" '{if(NF>1){x=$5-$7*3;if(x>=75){k=1}else{k=0};}if(k==1){print}}' > filtered.cds.fa
 cd-hit-est -c 1.0 -i filtered.cds.fa -M 0 -T 8 -o  combined.defrag.fa
 cd-hit-est -c 0.97 -i combined.defrag.fa -M 0 -T 8 -o  combined.clustered.fa # or run without above
 #time usearch9 -cluster_fast combined.defrag.fa -id 0.97 -centroids clutest.fa -sort length -maxrejects 4
 ## cd-hit wins by a distance (and isn't so slow)
 
-
-#usearch9 -makeudb_ublast combined.clustered.fa  -output db.udb # likely to very close to 32 bit usearch mem limit
-#usearch9 -ublast combined.clustered.fa -db db.udb -id 0.97 -evalue 1e-2 -accel 0.06 -strand plus -userout test.uo  -userfields query+target+clusternr+id+ql+tl+qcov+tcov+diffs+caln
+usearch9 -makeudb_ublast combined.clustered.fa -output db.udb #could be close to 32 bit usearch mem limit
+usearch9 -ublast combined.clustered.fa -db db.udb -id 0.97 -evalue 1e-2 -accel 0.06 -strand plus -userout res.uo  -userfields query+target+clusternr+id+ql+tl+qcov+tcov+diffs+caln
 #awk -F"\t" '{a=substr($1,length($1),1);b=substr($1,length($1)-4,1);if(a=="C"&&b!="-"){x="C"}else{x="I"};$(NF+1)=x;a=substr($2,length($2),1);b=substr($2,length($2)-4,1);if(a=="C"&&b!="-"){x="C"}else{x="I"};$(NF+1)=x;print}' OFS="\t" res2.uo >res.f.uo
-# then process in R 
+
+##### then process res.uo with filter.R to give transcripts.txt 
