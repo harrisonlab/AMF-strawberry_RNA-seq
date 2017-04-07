ade2d52bb8cb49a4a7b8468761010b5c5269b65a
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-04-07 16:09:44 +0100

    Update filter.R

--- filter.R
+++ filter.R
@@ -3,7 +3,7 @@ test <- fread("res2.uo") # read 21852707 rows and 8 (of 8) columns from 1.999 GB
 
 library(dplyr)
 test2 <- test
-dump_set <- subset(test2,V6>V5&V7>=100) # get rid of any subsequences (97% sequence identity over 100 length)
+dump_set <- subset(test2,V6>V5&V7>=50) # get rid of any subsequences (97% sequence identity over 50 length length)
 #test2<-anti_join(test2,dump_set)
 dump_set <- unique(dump_set,by="V1")
 test2<-anti_join(test2,dump_set[,1,with=F])
@@ -16,7 +16,7 @@ sames <- anti_join(sames,odd)
 test3 <- subset(test3,V7>=V8) # get subsequences of longer sequence
 primary <- unique(inner_join(sames, anti_join(sames[,1,with=F],test3[,1,with=F])))
 
-# get alternatives + one primary
+# get (well dump) alternatives + keep one primary
 a1 <- subset(test3,V5==V6&V7==V8)
 test3 <- anti_join(test3,a1)
 ## send anything with an alternative already in primary, to alternative 
@@ -26,33 +26,30 @@ a3 <- data.table(unique(inner_join(a1,a2)[,V1]))
 alternatives <- inner_join(a1,a3)
 a1 <- anti_join(a1,alternatives)
 setorder(a1,-V5)
-write.table(primary[,1,with=F],"primary.txt",row.names=F,col.names=F,quote=F)
-write.table(a1[,1:2,with=F],"alternative.txt",row.names=F,col.names=F,quote=F)
-
 e1 <- new.env()
 e2 <- new.env()
 sink("/dev/null")
-apply(a1,1, function(x) 
-  {y=(exists(x[1],envir=e1)*1);
-   y=y+(exists(x[2],envir=e1)*2);
-   y=y+(exists(x[1],envir=e2)*4);
-   y=y+(exists(x[2],envir=e2)*8);
-	if(y==0) {
-		e1[[x[1]]]="";
-		e2[[x[2]]]="";
-		#output.="$c[0]\n";	
-	} else if(y==2|y==8) {
-		e2[[x[1]]]=""
- 	} else if(y==1|y==4) {
-		e2[[x[2]]]=""
-	}   
-})  
+apply(a1,1,quickfunc)  
 sink(NULL)
-output <- data.table(ls(e1))
+primary2 <- data.table(ls(e1))
+alternatives2 <- data.table(ls(e2))
 rm(e1,e2)
+
+test4 <- anti_join(test3,primary2)
+test4 <- anti_join(test4,alternatives)
+test4 <- anti_join(test4,alternatives2)
+
+#write.table(primary[,1,with=F],"primary.txt",row.names=F,col.names=F,quote=F)
+#write.table(a1[,1:2,with=F],"alternative.txt",row.names=F,col.names=F,quote=F)
 write.table(output,"prim2.txt",col.names=F,row.names=F,quote=F)
 
-    
+quickfunc <- 
+function(x) {
+	y=(exists(x[1],envir=e1)*1)+(exists(x[2],envir=e1)*2)+(exists(x[1],envir=e2)*4)+(exists(x[2],envir=e2)*8);
+	if(y==0) {e1[[x[1]]]="";e2[[x[2]]]="";}
+	else if(y==2|y==8) {e2[[x[1]]]=""}
+	else if(y==1|y==4) {e2[[x[2]]]=""}
+}	
     
 ### OLD VERSION
 # for completes (these will be the primary list for main and alt) 
