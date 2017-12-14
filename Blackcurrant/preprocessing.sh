77c0c9442a5dd4cd565d28e4fd43192a0af6dda8
Greg Deakin <GregDeakin@users.noreply.github.com>
2017-12-14 10:47:22 +0000

    Create preprocessing.sh

---
+++ Blackcurrant/preprocessing.sh
@@ -0,0 +1,45 @@
+# create folders
+PROJECT_FOLDER=~/projects/blackcurrant
+
+ln -s  ~/pipelines/RNA-seq $PROJECT_FOLDER/RNA-seq_pipeline
+
+mkdir $PROJECT_FOLDER/cluster
+mkdir -p $PROJECT_FOLDER/data/fastq
+mkdir $PROJECT_FOLDER/data/trimmed
+mkdir $PROJECT_FOLDER/data/filtered
+mkdir $PROJECT_FOLDER/data/aligned
+mkdir $PROJECT_FOLDER/data/counts
+
+mkdir -p $PROJECT_FOLDER/analysis/quality
+mkdir $PROJECT_FOLDER/analysis/DGE
+mkdir $PROJECT_FOLDER/analysis/DEU
+
+ln -s /somesir/data/rna-seq/blackcurrent_project_xxx/data $PROJECT_FOLDER/data/fastq/.
+
+# quality chech
+for FILE in $PROJECT_FOLDER/data/fastq/*.gz; do 
+	$PROJECT_FOLDER/RNA-seq_pipeline/scripts/PIPELINE.sh -c qcheck $FILE $PROJECT_FOLDER/analysis/quality
+done
+
+# remove adapter contaminatation (not quality trimming)
+for FR in $PROJECT_FOLDER/data/fastq/*_1.fq.gz; do
+ RR=$(echo $FR|sed 's/\_1\.fastq/\_2\.fastq/')
+ $PROJECT_FOLDER/RNA-seq_pipeline/scripts/PIPELINE.sh -c trim \
+ $FR \
+ $RR \
+ $PROJECT_FOLDER/data/trimmed \
+ $PROJECT_FOLDER/RNA-seq_pipeline/scripts/truseq.fa \
+ 4
+done
+
+# filter phix etc. (this is memory intensive)
+for FR in $PROJECT_FOLDER/data/trimmed/*_1.fq.gz; do
+ RR=$(echo $FR|sed 's/\_1\.fq/\_2\.fq/')
+ $PROJECT_FOLDER/RNA-seq_pipeline/scripts/PIPELINE.sh -c filter \
+ $PROJECT_FOLDER/RNA-seq_pipeline/phix/phix \
+ $FR $RR \
+ $PROJECT_FOLDER/filtered
+done
+
+# build Salmon index
+salmon index -t $PROJECT_FOLDER/data/genome/transcriptome.fasta -i $PROJECT_FOLDER/data/genome/SALMON_quasi
