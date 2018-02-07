a7b9301ba00d2b9aa4f3c8b9a40127ee0c1d2941
Greg Deakin <GregDeakin@users.noreply.github.com>
2018-02-07 10:28:09 +0000

    Update annotate.sh

--- Blackcurrant/annotate.sh
+++ Blackcurrant/annotate.sh
@@ -1,1 +1,14 @@
- 
+
+# -appl CDD,COILS,Gene3D,HAMAP,MobiDBLite,PANTHER,Pfam,PIRSF,PRINTS,ProDom,PROSITEPATTERNS,PROSITEPROFILES,SFLD,SMART,SUPERFAMILY,TIGRFAM
+
+# annotation with interproscan
+~/pipelines/common/scripts/interproscan.sh \
+  /data/scratch/deakig/blackcurrant/genome \
+  ribes.protein.fasta \
+  /data/scratch/deakig/blackcurrant/genome/temp/
+
+# restarting interproscan
+ ~/pipelines/common/scripts/restart_interproscan.sh \
+  /data/scratch/deakig/blackcurrant/genome \
+  ribes.protein.fasta \
+  /data/scratch/deakig/blackcurrant/genome/temp/
